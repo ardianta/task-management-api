aaf08fcd698807058ec6e66b7e970315e99f12db
Dian <dian@petanikode.com>
2024-08-15 19:25:45 +0800

    feat: basic task management API
    - create new task
    - get all tasks
    - get task by ID
    - update task
    - delete task

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const { Task } = require("../models");
+const { where } = require('sequelize');
 
 /* GET tasks listing. */
 router.get('/', async function(req, res, next) {
@@ -11,14 +12,55 @@ router.get('/', async function(req, res, next) {
   res.json(tasks);
 });
 
+router.get('/:id', async function(req, res, next) {
+  const task = await Task.findByPk(req.params.id);
+  
+  if(!task){
+    return res.status(404).json({
+      message: "Task not found"
+    })
+  }
+
+  res.json(task);
+});
+
 router.post('/', async function(req, res, next) {
   
-  console.log(req.body)
-
   // TODO: validate data before storing
   const newTask = await Task.create(req.body);
 
   res.json(newTask);
 });
+
+router.put('/:id', async function(req, res, next) {
+  const oldTask = await Task.findByPk(req.params.id);
+  
+  if(!oldTask){
+    return res.status(404).json({
+      message: "Task not found"
+    })
+  }
+
+  // TODO: validate the data before storing
+  const newTask = req.body;
+  const updatedTask = await Task.update(newTask, {
+    where: { id: req.params.id }
+  })
+
+  res.json({ message: "Task updated!" });
+});
+
+router.delete('/:id', async function(req, res, next){
+  const task = await Task.findByPk(req.params.id);
+  
+  if(!task){
+    return res.status(404).json({
+      message: "Task not found"
+    })
+  }
+
+  Task.destroy({ where: { id: req.params.id } });
+  res.json({ message: "Task deleted!" });
+})
 
 module.exports = router;
